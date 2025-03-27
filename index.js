2dce3bcca1d5fcbcd4ad26074d5a840c4ee55c2e
jjmuchai <jjmuchai151@gmail.com>
2025-03-27 06:33:05 +0300

    My updates to the project

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+// Remove the existing <main> element with id "main"
+document.querySelector("main#main").remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement("h1");
+
+// Set its id to "victory"
+newHeader.id = "victory";
+
+// Set its text content
+newHeader.innerHTML = "James is the champion"; 
